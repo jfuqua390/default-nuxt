bac7ce405e6c22fe333e13e30c9febc57e7a2481
jfuqua390 <jfuqua390@gmail.com>
2023-04-15 14:13:46 -0400

    My default nuxt starter template

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  separator: "_",
+  theme: {
+    colors: {
+       // Using modern `rgb`
+       primary: 'rgb(var(--color-primary) / <alpha-value>)',
+       secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        serif: ['Romanesco', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/container-queries'),
+    require('tailwind-scrollbar')
+  ],
+};
